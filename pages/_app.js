570d8438059ea6807f65d9bbdb9aa10a5017ec2b
pearmini <subairui@icloud.com>
2025-10-17 14:34:45 -0400

    Split CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import "../styles/globals.css";
+import "../styles/Header.css";
+import "../styles/Hero.css";
+import "../styles/Features.css";
+import "../styles/Error.css";
 
 export default function App({Component, pageProps}) {
   return <Component {...pageProps} />;
