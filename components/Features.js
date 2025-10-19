6d3d50b67980bd1b8715e798ac99627333d8fa09
pearmini <subairui@icloud.com>
2025-10-19 17:34:52 -0400

    Update feature links

--- components/Features.js
+++ components/Features.js
@@ -11,22 +11,28 @@ export default function Features() {
 
   return (
     <ul className="features">
-      <li className="feature" onClick={handleNotebookClick}>
+      <li className="feature">
         <span className="feature-title">Recho Notebook</span>
         <span className="feature-description">A Reactive Editor for Algorithms and ASCII Art</span>
-        <img className="feature-image" src="/notebook.png" alt="Recho Notebook" style={{background: "#4269d0"}} />
+        <img
+          className="feature-image"
+          src="/notebook.png"
+          alt="Recho Notebook"
+          style={{background: "#4269d0"}}
+          onClick={handleNotebookClick}
+        />
       </li>
-      <li className="feature" onClick={handleComingSoon}>
+      <li className="feature">
         <span className="feature-title">Recho Multiples</span>
         <span className="feature-description">An Exploratory Editor for Creative Coding</span>
-        <span className="feature-image" style={{background: "#efb118"}}>
+        <span className="feature-image" style={{background: "#efb118"}} onClick={handleComingSoon}>
           <span className="feature-image-text">⏳ 🌲</span>
         </span>
       </li>
-      <li className="feature" onClick={handleComingSoon}>
+      <li className="feature">
         <span className="feature-title">Recho Melody</span>
         <span className="feature-description">A Chronological Editor for Coding with Music</span>
-        <span className="feature-image" style={{background: "#ff725c"}}>
+        <span className="feature-image" style={{background: "#ff725c"}} onClick={handleComingSoon}>
           <span className="feature-image-text">⏳ 🎵</span>
         </span>
       </li>
