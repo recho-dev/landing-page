fadae4115525f7f1be0e714d89e80f3e1325abbf
pearmini <subairui@icloud.com>
2025-10-20 23:26:12 -0400

    Add news for v0.2.0

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -4,18 +4,20 @@ import "../../styles/News.css";
 export default function News() {
   return (
     <div className="news-container">
-      {news.map((news) => (
-        <a href={news.link} target="_blank" rel="noreferrer" key={news.title} className="news-link">
-          <div className="news-item">
-            <h2 className="news-title">{news.title}</h2>
+      {news
+        .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt))
+        .map((news) => (
+          <div className="news-item" key={news.title}>
+            <a href={news.link} target="_blank" rel="noreferrer" className="news-link">
+              <h2 className="news-title">{news.title}</h2>
+            </a>
             <p className="news-meta">
               <span>{news.author}</span> • <span>{news.publishedAt}</span>
             </p>
             <p className="news-summary">{news.summary}</p>
             <img src={`news/${news.image}`} alt={news.title} className="news-image" />
           </div>
-        </a>
-      ))}
+        ))}
     </div>
   );
 }
